e2986f037ee4755b21349c9f05f85138f25f77d1
ryusun <ryusunshine@gmail.com>
2025-03-17 18:49:45 +0900

    feat:ryusun - 초기설정

---
+++ src/main/java/com/iruyeon/v1/IruyeonApplication.java
@@ -0,0 +1,13 @@
+package com.iruyeon.v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IruyeonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IruyeonApplication.class, args);
+    }
+
+}
